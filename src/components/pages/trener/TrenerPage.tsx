6ba2772310bd4abf8c14464ce3dfdfb3d2cd92c7
Marcin Zogrodnik <kontakt@marcinzogrodnik.pl>
2024-11-04 23:16:13 +0100

    trainer profile fixes

--- src/components/pages/trener/TrenerPage.tsx
+++ src/components/pages/trener/TrenerPage.tsx
@@ -8,6 +8,7 @@ import { Button } from '@/components/ui/button';
 import { formatPhoneNumber } from '@/lib/utils';
 // icons
 import {
+  EarthIcon,
   Facebook,
   Instagram,
   MailIcon,
@@ -56,61 +57,81 @@ const TrainerPage = ({ slug }: { slug: string }) => {
   } else {
     return (
       <Container
-        className="bg-white p-5 rounded-xl border border-[#E9EFFF] flex gap-10"
+        className="bg-white p-5 md:py-20 rounded-xl border border-[#E9EFFF] flex gap-10"
         width="max-w-5xl"
       >
-        <header className="flex flex-col md:flex-row md:gap-10 min-w-[300px] md:w-auto">
+        <header className="flex flex-col md:flex-row md:gap-10 lg:gap-14 min-w-[300px] md:w-auto md:items-center md:mx-auto">
           <div id="trainer_picture" className="relative">
             <Skeleton className="w-[150px] h-[150px] rounded-full" />
             <Button className="absolute -bottom-5">Polecany trener</Button>
           </div>
 
-          <div>
-            <h1 className="mt-7 font-medium text-lg">{trainer?.full_name}</h1>
-            <p className="text-sm">Trener personalny {trainer?.location}</p>
-            <p className="text-sm">stars (3 oceny)</p>
-          </div>
+          <div className="flex flex-col gap-4">
+            <div className="flex flex-col md:flex-row md:gap-10 lg:gap-14 md:justify-center">
+              <div>
+                <h1 className="mt-7 md:mt-0 font-medium text-lg">
+                  {trainer?.full_name}
+                </h1>
+                <p className="text-sm">Trener personalny {trainer?.location}</p>
+                <p className="text-sm">stars (3 oceny)</p>
+              </div>
 
-          <div className="mt-4 space-y-1">
-            <div className="flex gap-1 text-sm items-center">
-              <PhoneIcon width={20} />
-              {trainer?.phone}
-            </div>
-            <div className="flex gap-1 text-sm items-center">
-              <MailIcon width={20} />
-              {trainer?.email}
-            </div>
-          </div>
+              <div className="flex flex-col md:gap-1">
+                <div className="flex gap-1 text-sm items-center">
+                  <PhoneIcon width={20} />
+                  <a href={`tel:${trainer?.phone}`} className="font-medium">
+                    {trainer?.phone}
+                  </a>
+                </div>
+                <div className="flex gap-1 text-sm items-center">
+                  <MailIcon width={20} />
+                  <a href={`mailto:${trainer?.email}`} className="font-medium">
+                    {trainer?.email}
+                  </a>
+                </div>
+              </div>
 
-          <div className="mt-4 space-y-1">
-            <div className="flex gap-1 text-sm items-center">
-              <Instagram width={20} />
-              instagram
-            </div>
-            <div className="flex gap-1 text-sm items-center">
-              <Facebook width={20} />
-              facebook
-            </div>
-          </div>
-
-          <div className="mt-4 space-y-1">
-            <div className="flex flex-col gap-1 text-sm">
-              <p className="text-gray-600">Cena za godzinę</p>
-              <p className="text-base font-medium">{trainer?.price} zł</p>
-            </div>
-
-            <div className="flex flex-col gap-1 text-sm">
-              <p className="text-gray-600">Prowadzenie online</p>
-              <p className="text-base font-medium">
-                {trainer?.work_online ? 'Tak' : 'Nie'}
-              </p>
+              <div className="flex flex-col md:gap-1">
+                <div className="flex gap-1 text-sm items-center">
+                  <Instagram width={20} />
+                  <a href={`${trainer?.instagram}`} className="font-medium">
+                    {trainer?.instagram}
+                  </a>
+                </div>
+                <div className="flex gap-1 text-sm items-center">
+                  <Facebook width={20} />
+                  <a href={`${trainer?.facebook}`} className="font-medium">
+                    facebook
+                  </a>
+                </div>
+                <div className="flex gap-1 text-sm items-center">
+                  <EarthIcon width={20} />
+                  <a href={`${trainer?.www}`} className="font-medium">
+                    www
+                  </a>
+                </div>
+              </div>
             </div>
 
-            <div className="flex flex-col gap-1 text-sm">
-              <p className="text-gray-600">Plan dietetyczny</p>
-              <p className="text-base font-medium">
-                {trainer?.diet_plan ? 'Tak' : 'Nie'}
-              </p>
+            <div className="flex flex-col md:flex-row gap-10">
+              <div className="flex flex-col gap-1 text-sm">
+                <p className="text-gray-600">Cena za godzinę</p>
+                <p className="text-base font-medium">{trainer?.price} zł</p>
+              </div>
+
+              <div className="flex flex-col gap-1 text-sm">
+                <p className="text-gray-600">Prowadzenie online</p>
+                <p className="text-base font-medium">
+                  {trainer?.work_online ? 'Tak' : 'Nie'}
+                </p>
+              </div>
+
+              <div className="flex flex-col gap-1 text-sm">
+                <p className="text-gray-600">Plan dietetyczny</p>
+                <p className="text-base font-medium">
+                  {trainer?.diet_plan ? 'Tak' : 'Nie'}
+                </p>
+              </div>
             </div>
           </div>
         </header>
